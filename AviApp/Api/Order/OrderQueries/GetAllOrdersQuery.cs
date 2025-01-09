c15e48473ed44b43fa00c0c8f978e8f20b75c6a2
Avip2890 <avip2890@gmail.com>
2025-01-09 15:52:34 +0200

    fixed

--- AviApp/Api/Order/OrderQueries/GetAllOrdersQuery.cs
+++ AviApp/Api/Order/OrderQueries/GetAllOrdersQuery.cs
@@ -1,8 +1,11 @@
 using AviApp.Models;
 using MediatR;
+using System.Collections.Generic;
 
-namespace AviApp.Api.Order.OrderQueries;
-
-public class GetAllOrdersQuery : IRequest<IEnumerable<OrderDto>>
+namespace AviApp.Api.Order.OrderQueries
 {
+    public class GetAllOrdersQuery : IRequest<List<OrderDto>>
+    {
+ 
+    }
 }
--- AviApp/Api/Order/UpdateOrder/UpdateOrderCommandHandler.cs
+++ AviApp/Api/Order/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -1,5 +1,6 @@
 using AviApp.Interfaces;
 using AviApp.Models;
+using AviApp.Mappers;
 using MediatR;
 
 namespace AviApp.Api.Order.UpdateOrder;
@@ -9,17 +10,34 @@ public class UpdateOrderCommandHandler(IOrderService orderService) : IRequestHan
     public async Task<OrderDto?> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
     {
        
-        var updateOrderDto = new OrderDto
+        var existingOrderResult = await orderService.GetOrderByIdAsync(request.Id, cancellationToken);
+
+        if (!existingOrderResult.IsSuccess || existingOrderResult.Value == null)
         {
-            Id = request.Id,
-            OrderDate = request.OrderDate,
-            CustomerId = request.CustomerId,
-            Items = request.Items.Select(item => item.Id).ToList() 
-        };
+            return null; 
+        }
 
+        var existingOrder = existingOrderResult.Value;
+
+      
+        existingOrder.OrderDate = request.OrderDate;
+        existingOrder.CustomerId = request.CustomerId;
         
-        var updatedOrder = await orderService.UpdateOrderAsync(request.Id, updateOrderDto, cancellationToken);
+        var itemIds = request.Items.Select(item => item.Id);
+        var menuItemsResult = await orderService.GetMenuItemsByIdsAsync(itemIds, cancellationToken);
+        if (!menuItemsResult.IsSuccess || menuItemsResult.Value == null || !menuItemsResult.Value.Any())
+        {
+            throw new InvalidOperationException("Failed to fetch menu items for the order.");
+        }
+        existingOrder.Items = menuItemsResult.Value;
+        
+        var updatedOrderResult = await orderService.UpdateOrderAsync(request.Id, existingOrder, cancellationToken);
 
-        return updatedOrder;
+        if (!updatedOrderResult.IsSuccess || updatedOrderResult.Value == null)
+        {
+            return null; 
+        }
+        
+        return updatedOrderResult.Value.ToDto();
     }
 }
